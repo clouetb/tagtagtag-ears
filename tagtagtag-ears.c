4cd99fc0144de5e4bfb0e4ffd65ffcb8115b2c45
Benoît Clouet <benoit.clouet@gmail.com>
2020-10-26 23:47:07 +0100

    What about an irq ?

--- tagtagtag-ears.c
+++ tagtagtag-ears.c
@@ -854,7 +854,7 @@ static int init_ear(struct device *dev, struct tagtagtagear_data *priv, struct c
     timer_setup(&priv->broken_timer, tagtagtagear_broken_timer_cb, 0);
 
     // Request interrupts from encoder GPIOs
-    printk("After timer_setup priv %p encoder_gpio %p", priv, priv->encoder_gpio);
+    printk("After timer_setup priv %p encoder_gpio %p chip %p", priv, priv->encoder_gpio, priv->encoder_gpio->chip);
     irq = gpiod_to_irq(priv->encoder_gpio);
 
     printk("After gpiod_to_irq IRQ is %d", irq);
